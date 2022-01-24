14c24e50df87f2f10e3b3a5a8a590ded4a98117a
Andrei Sousa <andrei.sousa@einride.tech>
2022-01-24 17:54:34 +0100

    chore: migrate to mage-tools 0.44

--- .mage/magefile.go
+++ .mage/magefile.go
@@ -1,33 +1,21 @@
-//go:build mage
-// +build mage
-
 package main
 
 import (
+	"context"
+
+	"github.com/go-logr/logr"
 	"github.com/magefile/mage/mg"
+	"go.einride.tech/mage-tools/mglogr"
 	"go.einride.tech/mage-tools/mgmake"
 	"go.einride.tech/mage-tools/mgpath"
-
-	// mage:import
-	"go.einride.tech/mage-tools/targets/mgyamlfmt"
-
-	// mage:import
-	"go.einride.tech/mage-tools/targets/mgconvco"
-
-	// mage:import
-	"go.einride.tech/mage-tools/targets/mggo"
-
-	// mage:import
-	"go.einride.tech/mage-tools/targets/mggoreview"
-
-	// mage:import
-	"go.einride.tech/mage-tools/targets/mggolangcilint"
-
-	// mage:import
-	"go.einride.tech/mage-tools/targets/mgmarkdownfmt"
-
-	// mage:import
-	"go.einride.tech/mage-tools/targets/mggitverifynodiff"
+	"go.einride.tech/mage-tools/mgtool"
+	"go.einride.tech/mage-tools/tools/mgconvco"
+	"go.einride.tech/mage-tools/tools/mggit"
+	"go.einride.tech/mage-tools/tools/mggo"
+	"go.einride.tech/mage-tools/tools/mggolangcilint"
+	"go.einride.tech/mage-tools/tools/mggoreview"
+	"go.einride.tech/mage-tools/tools/mgmarkdownfmt"
+	"go.einride.tech/mage-tools/tools/mgyamlfmt"
 )
 
 func init() {
@@ -41,15 +29,63 @@ func init() {
 
 func All() {
 	mg.Deps(
-		mg.F(mgconvco.ConvcoCheck, "origin/master..HEAD"),
-		mggolangcilint.GolangciLint,
-		mggoreview.Goreview,
-		mggo.GoTest,
-		mgmarkdownfmt.FormatMarkdown,
-		mgyamlfmt.FormatYaml,
+		ConvcoCheck,
+		GolangciLint,
+		Goreview,
+		GoTest,
+		FormatMarkdown,
+		FormatYaml,
 	)
 	mg.SerialDeps(
-		mggo.GoModTidy,
-		mggitverifynodiff.GitVerifyNoDiff,
+		GoModTidy,
+		GitVerifyNoDiff,
 	)
+}
+
+func GitVerifyNoDiff(ctx context.Context) error {
+	ctx = logr.NewContext(ctx, mglogr.New("git-verify-no-diff"))
+	logr.FromContextOrDiscard(ctx).Info("verifying that git has no diff...")
+	return mggit.VerifyNoDiff(ctx)
+}
+
+func GoModTidy(ctx context.Context) error {
+	ctx = logr.NewContext(ctx, mglogr.New("go-mod-tidy"))
+	logr.FromContextOrDiscard(ctx).Info("tidying Go module files...")
+	return mgtool.Command(ctx, "go", "mod", "tidy", "-v").Run()
+}
+
+func Goreview(ctx context.Context) error {
+	ctx = logr.NewContext(ctx, mglogr.New("goreview"))
+	logr.FromContextOrDiscard(ctx).Info("running...")
+	return mggoreview.Command(ctx, "-c", "1", "./...").Run()
+}
+
+func GoTest(ctx context.Context) error {
+	ctx = logr.NewContext(ctx, mglogr.New("go-test"))
+	logr.FromContextOrDiscard(ctx).Info("running Go tests...")
+	return mggo.TestCommand(ctx).Run()
+}
+
+func GolangciLint(ctx context.Context) error {
+	ctx = logr.NewContext(ctx, mglogr.New("golangci-lint"))
+	logr.FromContextOrDiscard(ctx).Info("linting Go files...")
+	return mggolangcilint.RunCommand(ctx).Run()
+}
+
+func FormatMarkdown(ctx context.Context) error {
+	ctx = logr.NewContext(ctx, mglogr.New("format-markdown"))
+	logr.FromContextOrDiscard(ctx).Info("formatting Markdown files...")
+	return mgmarkdownfmt.Command(ctx, "-w", ".").Run()
+}
+
+func FormatYaml(ctx context.Context) error {
+	ctx = logr.NewContext(ctx, mglogr.New("format-yaml"))
+	logr.FromContextOrDiscard(ctx).Info("formatting YAML files...")
+	return mgyamlfmt.FormatYAML(ctx)
+}
+
+func ConvcoCheck(ctx context.Context) error {
+	ctx = logr.NewContext(ctx, mglogr.New("convco-check"))
+	logr.FromContextOrDiscard(ctx).Info("checking...")
+	return mgconvco.Command(ctx, "check", "origin/master..HEAD").Run()
 }
--- Makefile
+++ Makefile
@@ -14,39 +14,36 @@ clean-mage-tools:
 
 .PHONY: all
 all: $(magefile)
-	@$(magefile) all
+	@$(magefile) All
 
 .PHONY: convco-check
 convco-check: $(magefile)
-ifndef rev
-	$(error missing argument rev="...")
-endif
-	@$(magefile) convcoCheck $(rev)
+	@$(magefile) ConvcoCheck
 
 .PHONY: format-markdown
 format-markdown: $(magefile)
-	@$(magefile) formatMarkdown
+	@$(magefile) FormatMarkdown
 
 .PHONY: format-yaml
 format-yaml: $(magefile)
-	@$(magefile) formatYaml
+	@$(magefile) FormatYaml
 
 .PHONY: git-verify-no-diff
 git-verify-no-diff: $(magefile)
-	@$(magefile) gitVerifyNoDiff
+	@$(magefile) GitVerifyNoDiff
 
 .PHONY: go-mod-tidy
 go-mod-tidy: $(magefile)
-	@$(magefile) goModTidy
+	@$(magefile) GoModTidy
 
 .PHONY: go-test
 go-test: $(magefile)
-	@$(magefile) goTest
+	@$(magefile) GoTest
 
 .PHONY: golangci-lint
 golangci-lint: $(magefile)
-	@$(magefile) golangciLint
+	@$(magefile) GolangciLint
 
 .PHONY: goreview
 goreview: $(magefile)
-	@$(magefile) goreview
+	@$(magefile) Goreview
